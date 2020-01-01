b6157fa9c53c5dd0f76d71b6224262733037cad4
Joel Tok <jttctc@gmail.com>
2020-01-02 01:21:38 +0800

    refactor into the use of callback handlers

--- src/index.js
+++ src/index.js
@@ -6,37 +6,11 @@ async function sleep(milliseconds) {
   })
 }
 
-function initProgressTracker(upperBound) {
-  return {
-    success: 0,
-    failure: 0,
-    upperBound: upperBound,
-    incrementSuccess: function () {
-      this.success++;
-    },
-    incrementFailure: function() {
-      this.failure++;
-    },
-    summarize: function() {
-      console.log(`success: ${this.success}`)
-      console.log(`failure: ${this.failure}`)
-      console.log(`progress: ${this.success+this.failure} / ${this.upperBound}`)
-    },
-  }
-}
-
-// callbacks:
-// onSuccess
-// onFailure
-// onRetry
-// onBatchStart
-// onBatchEnd
-
-async function executeSingleItem(callback, retryFilters, progressTracker, { func, id }) {
+async function executeSingleItem(retryFilters, callbacks, { func, args }) {
+  const { onSuccess, onFailure, onRetry, ignoreFailures } = callbacks;
   try {
-    const result = await func()
-    await callback(result)
-    progressTracker.incrementSuccess()
+    const result = await func(...args)
+    await onSuccess(result)
     return {
       shouldRetry: false,
       value: result,
@@ -44,28 +18,35 @@ async function executeSingleItem(callback, retryFilters, progressTracker, { func
   } catch (err) {
     var shouldRetry = false;
     for (i = 0; i < retryFilters.length; i++) {
-
-      console.log('lll')
-      console.log(func)
-      console.log(err)
-
       if (retryFilters[i](err)) {
         shouldRetry = true;
         break;
       }
     }
     if (shouldRetry) {
-      console.log(`retrying one item...`)
+      await onRetry(args)
       return {
         shouldRetry: true,
-        value: { func, id },
+        value: { func, args },
       }
     } else {
-      progressTracker.incrementFailure()
-      console.error('Failure detected.')
-      console.error(`message: ${err.message}`)
-      console.error(`id: ${id}`)
-      console.error(`Not retrying.`)
+      // handling failures is a special case. without this, code fails silently
+      // without the user realising it.
+      if (onFailure) {
+        // user knows enough to add an onFailure handler
+        await onFailure(args, err);
+      } else if (ignoreFailures) {
+        // user knows enough to specify that they wish to ignore failures
+        // do nothing
+      } else if (!ignoreFailures && !onFailure) {
+        // user does not ignore failure, but also does not have a handler present
+        // default safeguard condition
+        console.error('One of your promises has failed. Normally this is handled by the onFailure error handler, but this is not defined in your code.')
+        console.error('Either')
+        console.error('(1) define an onFailure handler in options to handle all failures, or')
+        console.error('(2) set ignoreFailures: true in options to skip errors (not recommended).')
+        throw err;
+      }
       return {
         shouldRetry: false,
         value: null,
@@ -74,7 +55,7 @@ async function executeSingleItem(callback, retryFilters, progressTracker, { func
   }
 }
 
-module.exports = async (queuedPromises, callback, options) => {
+module.exports = async (queuedPromises, options) => {
   // put all promises into a queue,
   // execute N number at a time, every S seconds,
   // put failed ones into the back of the queue
@@ -82,15 +63,30 @@ module.exports = async (queuedPromises, callback, options) => {
     batchSize = 1,
     delayBetweenBatches = 1,
     retryFilters = [],
+    ignoreFailures = false,
+    onSuccess = () => {},
+    onFailure = null,
+    onRetry = () => {},
+    onBatchStart = () => {},
+    onBatchEnd = () => {},
     // strategy = 'naive', // naive / indexed
   } = options || {};
-  const progressTracker = initProgressTracker(queuedPromises.length)
+
+  const callbacks = {
+    onSuccess,
+    onFailure,
+    onRetry,
+    onBatchStart,
+    onBatchEnd,
+    ignoreFailures,
+  }
 
   while (queuedPromises.length) {
+    await onBatchStart()
     const currBatch = queuedPromises.splice(0, batchSize);
     const results = await Promise.all(
       currBatch.map(
-        executeSingleItem.bind(null, callback, retryFilters, progressTracker)
+        executeSingleItem.bind(null, retryFilters, callbacks)
       )
     )
     results.forEach(result => {
@@ -98,12 +94,10 @@ module.exports = async (queuedPromises, callback, options) => {
         queuedPromises.push(result['value'])
       }
     })
+    await onBatchEnd()
     if (queuedPromises.length <= 0) {
       break;
     }
-
-    console.log(`Batch summary`)
-    progressTracker.summarize()
     await sleep(delayBetweenBatches)
   }
   return true
--- src/index.spec.js
+++ src/index.spec.js
@@ -12,42 +12,41 @@ describe('index.spec.js', function () {
   var batchEndCount = 0;
 
   const successPromise = {
-    id: null,
-    func: () => {
+    func: (successValue) => {
       successCount++;
-      return Promise.resolve('successful value'); // this value is used by the onSuccess callback
-    }
+      return Promise.resolve(successValue); // this value is used by the onSuccess callback
+    },
+    args: ['successful value'],
   }
 
   const failurePromise = {
-    id: null,
-    func: () => {
+    func: (rejectedValue) => {
       failureCount++;
-      return Promise.reject(new Error('rejected'));
-    }
+      return Promise.reject(new Error('some-error-message'));
+    },
+    args: ['rejected'],
   }
 
   const retryPromiseGenerator = (retryTimes) => {
     var toRetry = retryTimes;
     return {
-      id: null,
-      func: () => {
+      func: (successValue, retryValue) => {
         if (toRetry <= 0) {
           successCount++;
-          return Promise.resolve('successful value')
+          return Promise.resolve(successValue)
         } else {
           toRetry --;
           retryCount++;
-          return Promise.reject(new Error('please retry'));
+          return Promise.reject(new Error(retryValue));
         }
-      }
+      },
+      args: ['successful value', 'retry value'],
     }
   }
 
-  const onSuccessCallback = async (result) => {
-    assert.equal(result, 'successful value')
-    return true
-  }
+  const retryFilters = [
+    (err) => err.message == 'retry value',
+  ]
 
   beforeEach(() => {
     successCount = 0;
@@ -62,7 +61,7 @@ describe('index.spec.js', function () {
       successPromise,
     ];
 
-    await batchPromisesWithRetry(promises, onSuccessCallback)
+    await batchPromisesWithRetry(promises)
     assert.equal(successCount, 3)
     assert.equal(retryCount,   0)
     assert.equal(failureCount, 0)
@@ -74,10 +73,30 @@ describe('index.spec.js', function () {
       failurePromise,
       failurePromise,
     ];
-    await batchPromisesWithRetry(promises, onSuccessCallback)
+    await batchPromisesWithRetry(promises, { ignoreFailures: true })
     assert.equal(successCount, 0)
     assert.equal(failureCount, 3)
     assert.equal(retryCount,   0)
+  })
+
+  it('all promises fail in execution without ignoreFailures set to true', async () => {
+    const promises = [
+      failurePromise,
+      failurePromise,
+      failurePromise,
+    ];
+    var hasError = false
+    try {
+      await batchPromisesWithRetry(promises)
+    } catch (err) {
+
+      console.log(err.message)
+
+      if (err.message == 'some-error-message') {
+        hasError = true
+      }
+    }
+    assert.equal(hasError, true)
   })
 
   it('some promises retry', async () => {
@@ -87,11 +106,7 @@ describe('index.spec.js', function () {
       retryPromiseGenerator(1),
     ]
 
-    const retryFilters = [
-      (err) => err.message == 'please retry',
-    ]
-
-    await batchPromisesWithRetry(promises, onSuccessCallback, { retryFilters })
+    await batchPromisesWithRetry(promises, { retryFilters })
     assert.equal(successCount, 3)
     assert.equal(failureCount, 0)
     assert.equal(retryCount,   5)
@@ -107,47 +122,129 @@ describe('index.spec.js', function () {
     ]
 
     const retryFilters = [
-      (err) => err.message == 'please retry',
+      (err) => err.message == 'retry value',
     ]
 
-    await batchPromisesWithRetry(promises, onSuccessCallback, { retryFilters })
+    await batchPromisesWithRetry(promises, { retryFilters, ignoreFailures: true })
     assert.equal(successCount, 3)
     assert.equal(failureCount, 2)
     assert.equal(retryCount,   8)
   })
 
   it('test use of batchSize and delayBetweenBatches options', async () => {
-    const options = {
-      batchSize: 2,
-      delayBetweenBatches: 10,
-    }
-
     const promises = [
       successPromise,
       successPromise,
       successPromise,
     ];
 
-    await batchPromisesWithRetry(promises, onSuccessCallback, options)
+    const options = {
+      batchSize: 2,
+      delayBetweenBatches: 10,
+    }
+
+    await batchPromisesWithRetry(promises, options)
     assert.equal(successCount, 3)
     assert.equal(retryCount,   0)
     assert.equal(failureCount, 0)
   })
 
-  it('test onSuccess, onFailure, onRetry, onBatchStart and onBatchEnd callbacks')
+  it('test onSuccess callbacks', async () => {
+    const promises = [
+      successPromise,
+      successPromise,
+      failurePromise,
+    ];
+
+    var onSuccessCount = 0;
+    const onSuccess = async (args, result) => {
+      onSuccessCount++;
+      assert.equal(args[0], 'successful value')
+      assert.equal(result, 'successful value')
+      return true;
+    }
+
+    await batchPromisesWithRetry(promises, { onSuccess, ignoreFailures: true })
+    assert.equal(successCount,   2)
+    assert.equal(retryCount,     0)
+    assert.equal(failureCount,   1)
+    assert.equal(onSuccessCount, 2)
+  })
+
+  it('test onFailure callbacks', async () => {
+    const promises = [
+      successPromise,
+      failurePromise,
+      failurePromise,
+    ];
+
+    var onFailureCount = 0;
+    const onFailure = async (args, err) => {
+      onFailureCount++;
+      assert.equal(args[0], 'rejected')
+      assert.equal(err.message, 'some-error-message')
+      return true;
+    }
+
+    await batchPromisesWithRetry(promises, { onFailure })
+    assert.equal(successCount,   1)
+    assert.equal(retryCount,     0)
+    assert.equal(failureCount,   2)
+    assert.equal(onFailureCount, 2)
+  })
+
+  it('test onRetry callbacks', async () => {
+    const promises = [
+      retryPromiseGenerator(1),
+      retryPromiseGenerator(3),
+      failurePromise,
+    ];
+
+    var onRetryCount = 0;
+    const onRetry = async (args) => {
+      onRetryCount++;
+      assert.equal(args[0], 'successful value')
+      return true;
+    }
+
+    await batchPromisesWithRetry(promises, { onRetry, retryFilters, ignoreFailures: true })
+    assert.equal(successCount,   2)
+    assert.equal(retryCount,     4)
+    assert.equal(failureCount,   1)
+    assert.equal(onRetryCount,   4)
+  })
+
+  it('test onBatchStart and onBatchEnd callbacks', async () => {
+    const promises = [
+      successPromise,
+      successPromise,
+      successPromise,
+      retryPromiseGenerator(3),
+      successPromise,
+      successPromise,
+    ];
+
+    var onBatchStartCount = 0;
+    const onBatchStart = async () => {
+      onBatchStartCount++;
+      return true;
+    }
+
+    var onBatchEndCount = 0;
+    const onBatchEnd = async () => {
+      onBatchEndCount++;
+      return true;
+    }
+
+    const batchSize = 2
+
+    await batchPromisesWithRetry(promises, { batchSize, retryFilters, onBatchStart, onBatchEnd })
+    assert.equal(successCount,   6)
+    assert.equal(retryCount,     3)
+    assert.equal(failureCount,   0)
+    assert.equal(onBatchStartCount, 6) // because the retry can only do one per batch if only one left
+    assert.equal(onBatchEndCount, 6)
+  })
+
+  it('test useRetryFilters options')
 })
-
-
-
-
-
-
-// const onbatchStartFunc = () => {
-//   batchStartCount++;
-//   return Promise.resolve;
-// }
-//
-// const onbatchEndFunc = () => {
-//   batchEndCount++;
-//   return Promise.resolve;
-// }
